d6fd7307973d88d157066f5bd2f61f996b906160
seedli <seedlix@gmail.com>
2017-01-25 18:16:27 +0800

    Add three sum

--- src/seed/leetcode/demo/ThreeSum.java
+++ src/seed/leetcode/demo/ThreeSum.java
@@ -13,31 +13,25 @@ public class ThreeSum {
     public static void main(String[] args){
         System.out.println(threeSum(new int[]{
 
-                5,-9,-11,9,9,-4,14,10,-11,1,-13,11,10,14,-3,-3,-4,6,-15,6,6,-13,7,-11,-15,10,-8,13,-14,-12,12,6,-6,8,0,10,-11,-8,-2,-6,8,0,12,3,-9,-6,8,3,-15,0,-6,-1,3,9,-5,-5,4,2,-15,-3,5,13,-11,7,6,-4,2,11,-5,7,12,-11,-15,1,-1,-9,10,-8,1,2,8,11,-14,-4,-3,-12,-2,8,5,-1,-9,-4,-3,-13,-12,-12,-10,-3,6,1,12,3,-3,12,11,11,10
+                13,9,1,12,-7,-12,7,3,9,6,-7,4,9,5,5,-7,4,11,1,-2,12,3,-12,-15,0,-12,-6,-1,7,-5,-4,-3,12,4,-14,-10,-1,8,1,-6,-1,9,13,-14,-1,-5,-6,-12,-8,2,2,11,13,-3,11,-2,1,-10,4,-15,-8,7,-11,11,-4,-10,-13,3,5,3,12,11,-11,2,12,3,13,13,-2,12,-7,-15,8,-9,-10,-4,-4,6,1,-15,-2,0,-1,2,-3,10,-1,-9,-10,-11,1,-13,-15,5,-3,5,-7,-5,-5,6,14,3,-1,7,1,-4,-12,12,-13,-4,4,0,3,-12,9,-15,6
 
         } ));
     }
 
-
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        int num1;
-        int num2;
-        int num3;
 
         for(int i=0; i< nums.length; i++){
-            num1=(nums[i]);
             for(int j=i+1; j<nums.length; j++){
-                num2 = nums[j];
                 for(int k=j+1; k<nums.length; k++){
-                    num3 = nums[k];
-                    if(num1 + num2 + num3 == 0){
-                        Integer[] tmpArray = new Integer[]{num1, num2, num3};
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        Integer[] tmpArray = new Integer[]{nums[i], nums[j], nums[k]};
                         List<Integer> tmpList = Arrays.asList(tmpArray);
-                        if(!checkIsExist(result, tmpList) ){
-                            result.add(new ArrayList<>(tmpList));
+                        if(result.size() > 0 && tmpList.equals(result.get(result.size()-1))){
+                            continue;
                         }
+                        result.add(new ArrayList<>(tmpList));
                     }
                 }
             }
