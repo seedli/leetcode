1eda0cc44d96be562b9c81b7dd6bb0e8a8d71378
seedli <seedlix@gmail.com>
2017-02-08 15:49:39 +0800

    2/8

---
+++ src/main/java/seed/leetcode/demo/A1_2Sum.java
@@ -0,0 +1,21 @@
+package seed.leetcode.demo;
+
+/**
+ * Created by User on 2017/2/8.
+ */
+public class A1_2Sum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for(int i = 0; i< nums.length; i++){
+            for(int j = i+1; j< nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
