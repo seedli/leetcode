8ec27f21fc1cd1ede3976061b4215c570dd0380b
seedli <seedlix@gmail.com>
2017-02-17 13:53:33 +0800

    A85

--- src/main/java/seed/leetcode/demo/A85_MaximalRectangle.java
+++ src/main/java/seed/leetcode/demo/A85_MaximalRectangle.java
@@ -7,27 +7,15 @@ import java.util.Arrays;
  */
 public class A85_MaximalRectangle {
 
-    //Not Finished
     public int maximalRectangle(char[][] matrix) {
-
+        if(matrix.length == 0) return 0;
         int[][] height = new int[matrix.length][matrix[0].length];
-        int[][] width = new int[matrix.length][matrix[0].length];
         int maxArea = 0;
 
         for(int Y=0; Y<matrix.length; Y++){
             for(int X=0; X<matrix[0].length; X++){
                 if(matrix[Y][X] != '0'){
-                    //Generate Width Array
-                    if(X==0 && matrix[Y][X] == '1'){
-                        width[Y][X] = 1;
-                    }else if(matrix[Y][X] == '1'){
-                        if(matrix[Y][X-1] == '1'){
-                            width[Y][X] = width[Y][X-1] + 1;
-                        }else{
-                            width[Y][X] = 1;
-                        }
-                    }
-                    //Generate Height Array
+                    //Generate Height 2D Array
                     if(Y==0 && matrix[Y][X] == '1'){
                         height[Y][X] = 1;
                     }else if(matrix[Y][X] == '1'){
@@ -40,16 +28,29 @@ public class A85_MaximalRectangle {
                 }
             }
         }
-        for(int[] row: width){
-            System.out.println(Arrays.toString(row));
+        for(int Y=0; Y<matrix.length; Y++) {
+            for (int X = 0; X < matrix[0].length; X++) {
+                maxArea = Math.max(getMaxArea(X,Y,height), maxArea);
+            }
         }
-        System.out.println("-----------");
         for(int[] row: height){
             System.out.println(Arrays.toString(row));
         }
 
         return maxArea;
+    }
 
+    public int getMaxArea(int X, int Y, int[][] height){
+        int max = 0;
+        int pointX = X;
+        int currentHeight = height[Y][X];
+
+        while(pointX >=0){
+            currentHeight = Math.min(height[Y][pointX], currentHeight);
+            max = Math.max(max, currentHeight*(X-pointX+1));
+            --pointX;
+        }
+        return max;
     }
 
 
