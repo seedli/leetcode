09f6df2d32ff76e3c52d0786db3370e5c34ea385
seedli <seedlix@gmail.com>
2017-03-09 22:18:54 +0800

    3/9

---
+++ src/main/java/seed/codility/Test1.java
@@ -0,0 +1,7 @@
+package seed.codility;
+
+/**
+ * Created by seedli on 2017/3/9.
+ */
+public class Test1 {
+}
---
+++ src/test/java/TestCodility.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import seed.codility.*;
+
+/**
+ * Created by seedli on 2017/3/9.
+ */
+public class TestCodility {
+    @Test
+    public void test1(){
+        Test1 test = new Test1();
+    }
+
+    @Test
+    public void test2(){
+        Test2 test = new Test2();
+    }
+
+    @Test
+    public void test3(){
+        Test3 test = new Test3();
+    }
+
+}
