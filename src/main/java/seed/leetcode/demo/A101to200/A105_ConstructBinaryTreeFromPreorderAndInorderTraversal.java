29f578429e4b9307b8c52de5988f33fd3b08a127
Tom Li <tom.li@sunbirddcim.com>
2021-12-02 22:24:40 +0800

    Challenge 105

--- src/main/java/seed/leetcode/demo/A101to200/A105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/seed/leetcode/demo/A101to200/A105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,5 +1,7 @@
 package seed.leetcode.demo.A101to200;
 
+import java.util.Arrays;
+
 import seed.leetcode.demo.TreeNode;
 
 /**
@@ -7,11 +9,27 @@ import seed.leetcode.demo.TreeNode;
  */
 public class A105_ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0) return null;
-        TreeNode root = new TreeNode(preorder[0]);
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (preorder.length == 0)
+			return null;
 
-        return null;
-    }
+		TreeNode root = new TreeNode(preorder[0]);
+		int rootIdx = findRootIndex(inorder, preorder[0]);
+
+		root.left = buildTree(Arrays.copyOfRange(preorder, 1, rootIdx + 1), Arrays.copyOfRange(inorder, 0, rootIdx));
+
+		root.right = buildTree(Arrays.copyOfRange(preorder, rootIdx + 1, preorder.length),
+				Arrays.copyOfRange(inorder, rootIdx + 1, inorder.length));
+
+		return root;
+	}
+
+	private int findRootIndex(int[] inorder, int rootVal) {
+		for (int i = 0; i < inorder.length; i++)
+			if (inorder[i] == rootVal)
+				return i;
+
+		return 0;
+	}
 
 }
