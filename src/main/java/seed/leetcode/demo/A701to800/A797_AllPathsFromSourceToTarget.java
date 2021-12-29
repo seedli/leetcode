b5026ef90dd74929c4c1e04a30a7089172aad3d9
Tom Li <tom.li@sunbirddcim.com>
2021-12-29 14:58:39 +0800

    challenge 797

---
+++ src/main/java/seed/leetcode/demo/A701to800/A797_AllPathsFromSourceToTarget.java
@@ -0,0 +1,31 @@
+package seed.leetcode.demo.A701to800;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A797_AllPathsFromSourceToTarget {
+
+	List<List<Integer>> result = new ArrayList<>();
+
+	public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+		result.clear();
+
+		dfs(0, graph, new ArrayList<Integer>());
+
+		return result;
+	}
+
+	private void dfs(int edge, int[][] graph, List<Integer> track) {
+		int[] adj = graph[edge];
+		track.add(edge);
+		if (edge == graph.length - 1) {
+			result.add(new ArrayList<Integer>(track));
+			return;
+		} else {
+			for (int i = 0; i < adj.length; i++) {
+				dfs(adj[i], graph, track);
+				track.remove(track.size() - 1);
+			}
+		}
+	}
+}
