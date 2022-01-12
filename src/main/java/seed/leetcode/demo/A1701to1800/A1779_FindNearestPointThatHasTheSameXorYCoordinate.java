1d1a96123b01f384d01c9437e14bdba706fe2a2a
Tom Li <tom.li@sunbirddcim.com>
2022-01-12 22:03:11 +0800

    1779 refine

--- src/main/java/seed/leetcode/demo/A1701to1800/A1779_FindNearestPointThatHasTheSameXorYCoordinate.java
+++ src/main/java/seed/leetcode/demo/A1701to1800/A1779_FindNearestPointThatHasTheSameXorYCoordinate.java
@@ -1,36 +1,24 @@
 package seed.leetcode.demo.A1701to1800;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class A1779_FindNearestPointThatHasTheSameXorYCoordinate {
 
 	public int nearestValidPoint(int x, int y, int[][] points) {
-		List<Integer> validIdxs = new ArrayList<>();
+		int nearestIdx = -1;
 
 		for (int i = 0; i < points.length; i++) {
 			if (points[i][0] == x && points[i][1] == y)
 				return i;
-			else if (points[i][0] == x || points[i][1] == y)
-				validIdxs.add(i);
-		}
+			else if (points[i][0] == x || points[i][1] == y) {
+				if (nearestIdx == -1) {
+					nearestIdx = i;
+				} else {
+					int d1 = manhattan(points[nearestIdx][0], points[nearestIdx][1], x, y);
+					int d2 = manhattan(points[i][0], points[i][1], x, y);
 
-		if (validIdxs.isEmpty())
-			return -1;
-
-		if (validIdxs.size() == 1)
-			return validIdxs.get(0);
-
-		int nearestIdx = validIdxs.get(0);
-		for (int i : validIdxs) {
-			if (i == nearestIdx)
-				continue;
-
-			int d1 = manhattan(points[nearestIdx][0], points[nearestIdx][1], x, y);
-			int d2 = manhattan(points[i][0], points[i][1], x, y);
-
-			if (d2 < d1)
-				nearestIdx = i;
+					if (d2 < d1)
+						nearestIdx = i;
+				}
+			}
 		}
 
 		return nearestIdx;
