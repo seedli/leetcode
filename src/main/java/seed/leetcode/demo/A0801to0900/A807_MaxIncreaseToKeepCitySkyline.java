c65d2e457db1dd0ec6e1f029633a698bbebff377
Tom Li <tom.li@sunbirddcim.com>
2022-01-20 17:24:03 +0800

    807 update

--- src/main/java/seed/leetcode/demo/A0801to0900/A807_MaxIncreaseToKeepCitySkyline.java
+++ src/main/java/seed/leetcode/demo/A0801to0900/A807_MaxIncreaseToKeepCitySkyline.java
@@ -8,10 +8,10 @@ public class A807_MaxIncreaseToKeepCitySkyline {
 		int[] maxHeightM = new int[grid.length];
 		int[] maxHeightN = new int[grid.length];
 
-		for (int m = 0; m < grid.length; m++) {
-			for (int n = 0; n < grid.length; n++) {
-				maxHeightM[m] = Math.max(maxHeightM[m], grid[m][n]);
-				maxHeightN[m] = Math.max(maxHeightN[m], grid[n][m]);
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				maxHeightM[i] = Math.max(maxHeightM[i], grid[i][j]);
+				maxHeightN[i] = Math.max(maxHeightN[i], grid[j][i]);
 			}
 		}
 
