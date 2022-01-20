9042abd414b199db90256af96cee239751e739f5
Tom Li <tom.li@sunbirddcim.com>
2022-01-20 18:48:01 +0800

    1262 TLE

--- src/main/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThree.java
+++ src/main/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThree.java
@@ -2,4 +2,37 @@ package seed.leetcode.demo.A1201to1300;
 
 public class A1262_GreatestSumDivisibleByThree {
 
+	private int sum = 0;
+
+	public int maxSumDivThree(int[] nums) {
+		sum = 0;
+		boolean[] visited = new boolean[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 3 == 0) {
+				sum += nums[i];
+				visited[i] = true;
+			}
+		}
+
+		backTrack(nums, visited, 0, sum);
+
+		return sum;
+	}
+
+	private void backTrack(int[] nums, boolean[] visited, int idx, int sum) {
+		if (sum % 3 == 0 && sum > this.sum)
+			this.sum = Math.max(this.sum, sum);
+
+		if (idx >= nums.length)
+			return;
+
+		// pick
+		if (!visited[idx]) {
+			backTrack(nums, visited, idx + 1, sum + nums[idx]);
+		}
+		// not to pick
+		backTrack(nums, visited, idx + 1, sum);
+	}
+
 }
--- src/test/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThreeTest.java
+++ src/test/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThreeTest.java
@@ -1,5 +1,7 @@
 package seed.leetcode.demo.A1201to1300;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class A1262_GreatestSumDivisibleByThreeTest {
@@ -7,5 +9,7 @@ public class A1262_GreatestSumDivisibleByThreeTest {
 	@Test
 	public void test() {
 		A1262_GreatestSumDivisibleByThree a = new A1262_GreatestSumDivisibleByThree();
+
+		assertEquals(18, a.maxSumDivThree(new int[] { 3, 6, 5, 1, 8 }));
 	}
 }
