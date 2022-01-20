e5323e958d70b4f0a0e18297d4b9f9ec2952735e
Tom Li <tom.li@sunbirddcim.com>
2022-01-20 18:26:35 +0800

    1262 WIP

---
+++ src/main/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThree.java
@@ -0,0 +1,5 @@
+package seed.leetcode.demo.A1201to1300;
+
+public class A1262_GreatestSumDivisibleByThree {
+
+}
---
+++ src/test/java/seed/leetcode/demo/A1201to1300/A1262_GreatestSumDivisibleByThreeTest.java
@@ -0,0 +1,11 @@
+package seed.leetcode.demo.A1201to1300;
+
+import org.junit.Test;
+
+public class A1262_GreatestSumDivisibleByThreeTest {
+
+	@Test
+	public void test() {
+		A1262_GreatestSumDivisibleByThree a = new A1262_GreatestSumDivisibleByThree();
+	}
+}
